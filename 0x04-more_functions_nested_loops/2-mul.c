22371c467559205c6685a58066feae5baaf027c6
jamiu2021 <jamiu.olaide1991@gmail.com>
2023-06-23 16:34:47 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+/**
+ * mul -  multiplies two integers
+ * @a & @b: int to multiply
+ * Return:multiplication of integer
+ */
+
+int mul(int a, int b)
+{
+	return(a * b)
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio>
+/**
+ * print_numbers - to print 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+	for(c='0';c<='9';c++)
+		_putchar(c);
+}
+		_putchar('\n');
+}
